a0c86336c46ea610bd75d5a83667fd807a59dcfa
Vipin <vipin251@gmail.com>
2015-04-26 16:24:54 +0530

    Final r code

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+#Require dplyr and data.table packages
 library(dplyr)
 library(data.table)
 #read  features and labels 
@@ -35,10 +36,10 @@ for(i in labels$V1){
         merged_df$activity <- gsub( i, labels[i, 2], merged_df$activity)
 }
 #remove unwanted intermediate files from env
-rm( "x_subject_test", "x_subject_train","x_test","x_train","y_test","y_train", 
-    "final_without_colnames", "test_with_activity_id", "test_with_suject_id", 
-    "train_with_activity_id", "train_with_subject_id", "i", "subset_mean_std", 
-    "select_means", "features",  "labels")
+rm("x_subject_test", "x_subject_train","x_test","x_train","y_test","y_train", 
+   "final_without_colnames", "test_with_activity_id", "test_with_suject_id", 
+   "train_with_activity_id", "train_with_subject_id", "i", "subset_mean_std", 
+   "features",  "labels")
 
 #Find mean of each variable for each activity and each subject
 tidy_data_means <- data.frame()
@@ -49,16 +50,21 @@ tidy_data_means <- rbind(tidy_data_means, temp)
 }
 #Clean column names and make it descriptive and lower case
 bad_colnames <- names(tidy_data_means)
-bad_colnames <- gsub("^t", "time", bad_colnames)
-bad_colnames <- gsub("^f", "freequency", bad_colnames)
-bad_colnames <- gsub("std", "standarddeviation", bad_colnames)
-bad_colnames <- gsub("Acc", "acceleration", bad_colnames)
-bad_colnames <- gsub("Gyro", "gyroscope", bad_colnames)
-bad_colnames <- gsub(",", "", bad_colnames)
+bad_colnames <- gsub("^t", "Time", bad_colnames)
+bad_colnames <- gsub("^f", "Freequency", bad_colnames)
+bad_colnames <- gsub("std", "StandardDeviation", bad_colnames)
+bad_colnames <- gsub("Acc", "Acceleration", bad_colnames)
+bad_colnames <- gsub("Gyro", "Gyroscope", bad_colnames)
 bad_colnames <- gsub(",", "", bad_colnames)
 bad_colnames <- gsub("[\\()]", "", bad_colnames)
 bad_colnames <- gsub("-", "", bad_colnames)
-bad_colnames <- tolower(bad_colnames)
+bad_colnames <- gsub("Mag", "Magnitude", bad_colnames)
+bad_colnames <- gsub("mean", "Mean", bad_colnames)
+bad_colnames <- gsub("Freq", "Freequency", bad_colnames)
+bad_colnames <- gsub("anglet", "AngleTime", bad_colnames)
+bad_colnames <- gsub("angle", "Angle", bad_colnames)
+bad_colnames <- gsub("gravity", "Gravity", bad_colnames)
+#bad_colnames <- tolower(bad_colnames)
 setnames(tidy_data_means, names(tidy_data_means), bad_colnames)
 #Re-arranging the columns
 subject <- tidy_data_means[,2, with = F]
@@ -70,8 +76,8 @@ View(tidy_data_means)
 write.table(tidy_data_means, file = "tidy_data.txt", row.names = F)
 
 #To the peer reviewer: code to read the output file produced by run_analysis.R
-#is given below. Please remove the "#" symbol from the below line and run the 
-#code
+#is given below. Please remove the "#" symbol from the beggining  below line and run the 
+#command to get the output file on your console
 
-tidy_output <- read.table("tidy_data.txt", header = T); View (tidy_output)
+#tidy_output <- read.table("tidy_data.txt", header = T); View (tidy_output)
 
